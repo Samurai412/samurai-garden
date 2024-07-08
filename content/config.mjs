b7a91f2628fa942358ede94a5e1657189dbcb693
Abdallah Suleiman Sulaiman Arabi <101192366+Samurai412@users.noreply.github.com>
2024-07-08 21:24:08 +0300

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -6,7 +6,7 @@ const config = {
   showSidebar: true,
    title: "Samurai Garden",
   showEditLink: true,
-  editLinkRoot: "https://github.com/Samurai412/samurai-garden/edit/main/site,
+  editLinkRoot: "https://github.com/Samurai412/samurai-garden/edit,
   navLinks: [{ href: "/blog", name: "Blog" }],
 
 };
