b196a8a01e8a0e77a148e17bc2ef4e3cf25614bc
Franz Bonaparta <101192366+Samurai412@users.noreply.github.com>
2025-02-02 16:37:26 +0300

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -7,8 +7,7 @@ const config = {
    title: "Samurai Garden",
   showEditLink: true,
   editLinkRoot: "https://github.com/Samurai412/samurai-garden/edit/main/",
-  navLinks: [{ href: "/blog", name: "Blog" },{ href: "/Creative posting/Flash fiction", name: "Creative shit"}                 
-            ],
+  navLinks: [{ href: "/blog", name: "Blog" }],
 
 };
 
